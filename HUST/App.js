17da88578d33119ea0b04f5b4d83e4d4f172f563
Kiên Nguyễn <nguyenphuctrungkien@gmail.com>
2017-05-29 14:24:53 +0700

    Add react-redux

---
+++ HUST/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import TestFireBase from './src/components/TestFirebase'
+
+class App extends Component {
+   
+    render() {
+        return (
+            <View></View>
+        );
+    }
+}
+
+export default App;
